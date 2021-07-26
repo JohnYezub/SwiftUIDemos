c81122c613f019a0cf8d32bacba5ad104cbe566d
Evgeny <buze@ya.ru>
2021-07-26 20:10:11 +0800

    Initial Commit

---
+++ SwiftUIDemos/SwiftUIDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemosApp.swift
+//  SwiftUIDemos
+//
+//  Created by Evgeny on 26.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
